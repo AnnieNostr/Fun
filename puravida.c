f08c38e340bd0acfbf0c1aecb4dc6c0b46d44cbe
AnnieNostr <150483599+AnnieNostr@users.noreply.github.com>
2023-11-10 15:01:59 -0600

    First code in C
    Test cod that will print "pura vida" and the letters P and V.

---
+++ puravida.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    printf("pura vida\n");
+    {
+        printf("### #   #\n");
+    }
+    {
+        printf("# # #   #\n");
+    }
+    {
+        printf("###  # #\n");
+    }
+    {
+        printf("#    # #\n");
+    }
+    {
+        printf("#    # #\n");
+    }
+    {
+        printf("#     #\n");
+    }
+    printf("\n");
+}
